17516d4fda00a7df35bce1cd104e8947b5c4e265
lalit-dev <lalityadav.mail@gmail.com>
2020-07-18 02:38:34 +0530

    v1 implemented

---
+++ src/db/mongoose.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+
+if(!process.env.MONGODB_URL){
+    process.env.MONGODB_URL = "mongodb://127.0.0.1:27018"
+}
+
+mongoose.connect(process.env.MONGODB_URL, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+})
---
+++ src/emails/account.js
@@ -0,0 +1,34 @@
+const sgMail = require('@sendgrid/mail')
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+
+const sendWelcomeEmail = (email, name) => {
+    sgMail.send({
+        to: email,
+        from: process.env.SENDGRID_FROM_MAIL,
+        subject: 'Thanks for joining in!',
+        text: `Welcome to the app, ${name}. Let me know how you get along with the app.`
+    }).then(res => {
+        console.log("[ sendWelcomeEmail ] response: ", res);
+    }).catch(err => {
+        console.log("[ sendWelcomeEmail ] error: ", err.response.body);
+    })
+}
+
+const sendCancelationEmail = (email, name) => {
+    sgMail.send({
+        to: email,
+        from: process.env.SENDGRID_FROM_MAIL,
+        subject: 'Sorry to see you go!',
+        text: `Goodbye, ${name}. I hope to see you back sometime soon.`
+    }).then(res => {
+        console.log("[ sendCancelationEmail ] response: ", res);
+    }).catch(err => {
+        console.log("[ sendCancelationEmail ] error: ", err.response.body);
+    })
+}
+
+module.exports = {
+    sendWelcomeEmail,
+    sendCancelationEmail
+}
